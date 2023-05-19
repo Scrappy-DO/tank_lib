1a91031c30a8249f5829457ed8766d393f4ba1ab
scrappyDO <scrappy.deoverlever@gmail.com>
2023-05-19 14:20:36 +0200

    finaly fixed l298n lib bug

--- src/Tank.cpp
+++ src/Tank.cpp
@@ -1,33 +1,39 @@
 #include "Arduino.h"
 #include "Tank.h"
-#include <L298n.h>
+#include "L298n.h"
 
 TankLib::Tank::Tank(int enL, int in1L, int in2L, int enR, int in1R, int in2R, bool invertL, bool invertR){
 	en_L = enL;
 	in1_L = in1L;
 	in2_L = in2L;
-	invert_L = invertL
+	invert_L = invertL;
 
 	en_R = enR;
 	in1_R = in1R;
 	in2_R = in2R;
 	invert_R = invertR;
 
-	L298n::Driver motor(en_L, en_R, in1_L, in2_L, in1_R, in2_R, invert_L, invert_R);
+	motor.setPins(en_L, en_R, in1_L, in2_L, in1_R, in2_R);
+	if(invert_L){
+		motor.invertA();
+	}
+	if(invert_R){
+		motor.invertB();
+	}
 }
 
-TankLib::Tank::begin(){
+void TankLib::Tank::begin(){
 	motor.begin();
 }
 
-TankLib::Tank::drive(bool dirL, bool dirR, int speedL, int speedR){
+void TankLib::Tank::drive(bool dirL, bool dirR, int speedL, int speedR){
 
 }
 
-TankLib::Tank::driveDir(int _dir, int _speed){
+void TankLib::Tank::driveDir(int _dir, int _speed){
 
 }
 
-TankLib::Tank::stop(){
+void TankLib::Tank::stop(){
 
 }
